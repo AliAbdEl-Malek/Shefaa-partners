e6df70509f4277bc6e29c64e0466ec3a944ac427
Ali Abd El-Malek <aliabdelmalek90@gmail.com>
2021-02-12 17:42:16 +0200

    complete registeration module and components

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,28 +2,30 @@
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PartnerService } from './partner.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private httpClient:HttpClient, ) {
+  constructor(private httpClient:HttpClient, private _partnerService:PartnerService ) {
 
   }
 
-   
+  token:any = this._partnerService.getToken()
+
 
   get(url:string){
-    return this.httpClient.get(`${environment.APIURL}/${url}`);
+    return this.httpClient.get(`${environment.APIURL}/${url}`,{headers:{'authorization':this.token}});
   }
 
-  post(url:string,user:any){
-    return this.httpClient.post(`${environment.APIURL}/${url}`,user)
+  post(url:string,partner:any){
+    return this.httpClient.post(`${environment.APIURL}/${url}`,partner)
   }
 
-  put(url:string,user:any){
-    return this.httpClient.put(`${environment.APIURL}/${url}`,user)
+  put(url:string,partner:any){
+    return this.httpClient.put(`${environment.APIURL}/${url}`,partner,{headers:{'authorization':this.token}})
   }
 
   
